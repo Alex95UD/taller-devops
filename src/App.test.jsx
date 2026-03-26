29fc42cae1a5c1e791adb5a64d7ab3910ca0dc4e
Alex95UD <dimunozr@udistrital.edu.co>
2026-03-26 02:07:53 +0000

    ajuste de test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-// PRUEBA 1: Verifica que el título existe en el DOM
 test('muestra el título de la app', () => {
   render(<App />)
-  const titulo = screen.getByText(/Hola DevOps/i)
+  const titulo = screen.getByRole('heading', {
+    level: 1,
+    name: /aplicación devops/i
+  })
   expect(titulo).toBeInTheDocument()
 })
 
-// PRUEBA 2: Verifica que el mensaje de despliegue existe
 test('muestra el mensaje de despliegue', () => {
   render(<App />)
-  const mensaje = screen.getByText(/GitHub Actions/i)
+  const mensaje = screen.getByText(/Desplegado con GitHub Actions/i)
   expect(mensaje).toBeInTheDocument()
 })
 
-// PRUEBA 3: Verifica que el título es un elemento h1
 test('el título principal es un h1', () => {
   render(<App />)
-  const h1 = document.querySelector('h1')
+  const h1 = screen.getByRole('heading', { level: 1 })
   expect(h1).toBeInTheDocument()
 })
